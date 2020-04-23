7482cac1eb75c38e0efa3477ebf68d393f69e507
Milon9787 <56488424+Milon9787@users.noreply.github.com>
2020-04-23 13:47:27 +0600

    left sidebar

--- index.php
+++ index.php
@@ -16,6 +16,115 @@
 <body>
 
 
+<header class="header">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="hdr-innr clearfix">
+          <div class="hdr-lft">
+            <div class="logo">
+              <a href="#">
+                <img src="assets/images/logo.png">
+              </a>
+            </div>
+          </div>
+          <div class="hdr-rgt">
+            <div class="hdr-contact">
+              <span>CONTACT CUSTOMER SERVICE</span>
+              <a href="tel:(800) 305-1445">(800) 305-1445</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<section class="sns-content-sec-wrp">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="sns-content-block clearfix">
+          <div class="sns-content-lft">
+            <div class="sns-order-timer">
+              <h3 class="sns-order-timer-title">Your order is reserved for: <span>0:00</span></h3>
+            </div>
+            <div class="sns-availability-wrp">
+              <div class="sns-availability">
+                <p>Current Avaliability</p>
+                <strong>Low</strong>
+                <p>Sell Out Risk <span>High</span></p>
+              </div>
+            </div>
+            <div class="sns-product-dsc-wrp">
+              <div class="sns-product-dsc">
+                <p><b>Great Job!</b> You’re taking your first step towards a healthier You. Act now so you don’t miss out on this opportunity!</p>
+              </div>
+              <div class="sns-product-price-dsc">
+                <h4 class="sns-product-price-title-1">YOUR PACKAGE DETAILS</h4>
+                <h3 class="sns-product-price-title-2">Bioharmony Advance</h3>
+                <span>Original Price: $149.00</span>
+                <strong>Discounted Price: $49.00</strong>
+              </div>
+              <div class="sns-product-img">
+                <img class="lazy" data-src="assets/images/sns-product-img.png">
+              </div>
+            </div>
+            <div class="sns-money-back-dsc-wrp clearfix">
+              <div class="sns-money-back-dsc-tp clearfix">
+                <div class="sns-money-back-img">
+                  <img class="lazy" data-src="assets/images/moneybackcheck.png">
+                </div>
+                <div class="sns-money-back-dsc">
+                  <span>180 DAY BULLET PROOF GUARANTEE</span>
+                  <h2 class="sns-money-back-title">100% Money Back</h2>
+                  <p>After you order any of our products today, if you are not completely satisfied, simply contact me within the next half a year and send it back in any condition and I will see that you get a complete refund with no questions asked, no hassle whatsoever.</p>
+                </div>
+              </div>
+              <div class="sns-money-back-dsc-btm">
+                <p>That’s 180 days to experience the amazing future you’ve been reading about. If you are wondering why I am doing this the answer is simple. I believe in these products so much I am taking ALL THE RISK so you will start living healthier today. </p>
+              </div>
+            </div>
+          </div>
+          <div class="sns-content-rgt"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="footer-sec-wrp">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="ftr-innr clearfix">
+          <div class="ftr-tp-dsc clearfix">
+            <div class="ftr-lft">
+              <a href="#">
+                <img src="assets/images/ftr-logo.png">
+              </a>
+            </div>
+            <div class="ftr-rgt">
+              <div class="ftr-menu">
+                <ul class="clearfix">
+                  <li><a href="#">Refunds</a></li>
+                  <li><a href="#">Privacy Policy</a></li>
+                  <li><a href="#">Terms</a></li>
+                  <li><a href="#"> Contact</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="ftr-btm-dsc">
+            <p>"Statements on this website have not been evaluated by the Food and Drug Administration. Products are not intended to diagnose, treat, cure or prevent any disease. Review all product warnings prior to use." "Not intended to cure any disease". "Product is not scientifically proven with 2 double-blind randomized controlled tests". The information provided on this site is for informational purposes only and is not intended as a substitute for advice from your physician or other health care professional or any information contained on or in any product label or packaging. You should not use the information on this site for diagnosis or treatment of any health problem or for prescription of any medication or other treatment. Always consult with a healthcare professional before starting any diet, exercise or supplementation program, before taking any medication, or if you have or suspect you might have a health problem. You should not stop taking any medication without first consulting your physician. Results featured on this web site are atypical. As individuals differ, so will results. Science Natural Supplements distributes a product line made with natural ingredients. Always check with your doctor for risks associated with dietary supplements and your specific health conditions and/or allergies. Representations regarding the efficacy and safety of these products have not been evaluated by the Food and Drug Administration. The FDA only evaluates foods and drugs, not supplements like these products. These products are not intended to diagnose, prevent, treat, or cure any disease. The product is not scientifically proven with 2 double-blind randomized controlled tests. "(C) Science Natural Supplements, 304 S. Jones Blvd #1878 Las Vegas, Nevada 89107, Las Vegas, Nevada, 89017, United States 1 (800) 305-1445 - support@sciencenaturalsupplements.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
 <script src="assets/js/jquery-plugins.js"></script>
 <script src="assets/js/main.js"></script>
 <script type="text/javascript">window.onload = function() {var newd1 = new LazyLoad({elements_selector: ".lazy"});}</script>
