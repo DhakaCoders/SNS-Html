6a60fb97eb7a34ef6f796c1aa6897aca8d0feee9
Foridul Islam <foridulsumon@gmail.com>
2020-04-23 09:09:17 +0600

    Init

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+	<title>Science Natural Wellness</title>
+  <meta name="robots" content="noindex,nofollow">
+  <meta name="robots" content="noindex">
+  <meta name="robots" content="nofollow">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome/fontawesome.min.css"> 
+  <link href="https://fonts.googleapis.com/css?family=Heebo:400,700,800&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
+<script src="assets/js/jquery-plugins.js"></script>
+<script src="assets/js/main.js"></script>
+<script type="text/javascript">window.onload = function() {var newd1 = new LazyLoad({elements_selector: ".lazy"});}</script>
+</body>
+</html>
